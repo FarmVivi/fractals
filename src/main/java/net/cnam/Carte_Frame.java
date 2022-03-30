b846d15e252a223284ff81de43e5d6181330ebb1
Alban <alban.rouault@hotmail.com>
2022-03-30 13:17:25 +0200

    le travail qu'on a fait ensemble
    Co-authored-by: FarmVivi <FarmVivi@users.noreply.github.com>

--- src/main/java/net/cnam/Carte_Frame.java
+++ src/main/java/net/cnam/Carte_Frame.java
@@ -1,14 +1,12 @@
 package net.cnam;
 
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
 
 public class Carte_Frame extends JFrame {
     private Carte_Panel panel;
 
     public Carte_Frame(App app) {
-        panel = new Carte_Panel();
+        panel = new Carte_Panel(3, 100, 100, 62548759613L, 128);
 
         // Caractéristiques de la fenêtre
         setTitle("Fractals - Carte");
@@ -18,12 +16,5 @@ public class Carte_Frame extends JFrame {
 
         // Ajout du panel
         this.add(panel);
-
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                panel.getTimer().stop();
-            }
-        });
     }
 }
--- src/main/java/net/cnam/Carte_Panel.java
+++ src/main/java/net/cnam/Carte_Panel.java
@@ -1,56 +1,97 @@
 package net.cnam;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.Random;
+
 import javax.swing.JPanel;
-import javax.swing.Timer;
 
-public class Carte_Panel extends JPanel implements ActionListener {
-    private final int DELAY = 150; // en ms
-    private Timer timer;
+public class Carte_Panel extends JPanel {
 
-    public Carte_Panel() {
-        initTimer();
+    // CHAMPS
+    private int pas; // p
+    private int maille; // m
+    private int hauteur; // h
+    private int deviation; // d
+    private int taille; // l (à voir si on met des limites (128, 64, 32))
+    private int n; // à voir son utilité plus tard
+
+    private Random random; // z
+
+    // CONSTRUCTEUR
+    public Carte_Panel(int maille, int hauteur, int deviation, long graine, int taille)
+            throws IllegalArgumentException {
+        if (maille < 0 || maille > 3)
+            throw new IllegalArgumentException("maille doit être compris entre 0 et 3");
+
+        this.maille = maille;
+        this.pas = (int) Math.pow(2, 7 - this.maille);
+        System.out.println("Pas: " + this.pas);
+        this.hauteur = hauteur;
+        this.deviation = deviation;
+        this.taille = taille;
+
+        this.random = new Random(graine);
+
+        this.n = this.hauteur / 16;
+
+        repaint();
     }
 
-    private void initTimer() {
-        timer = new Timer(DELAY, this);
-        timer.start();
+    public void affichage() {
+        this.repaint();
     }
 
-    public Timer getTimer() {
-        return timer;
-    }
+    // ligne 290 - 350 ||| revoir le pas
+    public void surfaceDeBase(Graphics g) {
 
-    private void doDrawing(Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
+        int[][] surface = new int[taille][taille];
 
-        g2d.setPaint(Color.blue);
+        for (int x = 0; x < this.taille; x += this.pas) {
+            for (int y = 0; y < this.taille; y += this.pas) {
+                surface[x][y] = random.nextInt() * this.hauteur;
+                if (surface[x][y] < this.n) {
+                    surface[x][y] = this.n;
+                }
 
-        int w = getWidth();
-        int h = getHeight();
+                int c = surface[x][y] / this.n;
+                if (c > 15) {
+                    c = 15;
+                }
 
-        Random r = new Random();
-
-        for (int i = 0; i < 2000; i++) {
-            int x = Math.abs(r.nextInt()) % w;
-            int y = Math.abs(r.nextInt()) % h;
-            g2d.drawLine(x, y, x, y);
+                plot(g, x * 4, y * 2, new Color(c * 17, c * 17, c * 17));
+            }
         }
+
+        calculFractal(surface);
+    }
+
+    // on s'est arrêté dans se gros bourbier rpz la ligne 410
+    public void calculFractal(int[][] h) {
+        while (this.pas>1) {
+            
+            int q = this.pas / 2;
+            int e = this.deviation / 2;
+
+            for (int x = q ; x < this.taille - q ; x += this.pas){
+                for (int y = q ; y < this.taille - q ; y += this.pas){
+                    this.hauteur = (h[h[x-q,y-q]+h[x-q,y+q]+h[x+q,y-q]+h[x+q,y+q]]) / 4;
+                }
+            }
+        }
+    }
+
+    public void plot(Graphics g, int x, int y, Color c) {
+        System.out.println("x" + x + " y" + y + " c" + c.getRGB());
+        g.setColor(c);
+        g.drawLine(x, y, x, y);
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        doDrawing(g);
-    }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        repaint();
+        surfaceDeBase(g);
     }
 }
