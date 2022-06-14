e72882d6bf7bca6264d2bac9cea8f3ab0aea3242
Alban <alban.rouault@hotmail.com>
2022-06-14 19:23:39 +0200

    modification de FractalSettings (pb avec le modulo 2 fixed)

--- src/main/java/net/cnam/fractals/FractalsSettings.java
+++ src/main/java/net/cnam/fractals/FractalsSettings.java
@@ -70,7 +70,7 @@ public class FractalsSettings {
         if (deviation < 0) {
             throw new IllegalArgumentException("deviation must be positive");
         }
-        if (taille <= 0 || taille % 2 == 0) {
+        if (taille <= 0 || taille % 2 != 0) {
             throw new IllegalArgumentException("taille doit être supérieur à 0 et un multiple de 2");
         }
     }
