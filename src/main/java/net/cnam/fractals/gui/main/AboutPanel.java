e5fa49dae52963b03b27539680c85bc97deb6d85
Alban <alban.rouault@hotmail.com>
2022-06-14 19:17:09 +0200

    modification de AboutPanel

--- src/main/java/net/cnam/fractals/gui/main/AboutPanel.java
+++ src/main/java/net/cnam/fractals/gui/main/AboutPanel.java
@@ -3,23 +3,62 @@ package net.cnam.fractals.gui.main;
 import net.cnam.fractals.utils.Utils;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
 
 public class AboutPanel extends JPanel {
     public AboutPanel() {
+
         this.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.anchor = GridBagConstraints.WEST;
-        gbc.gridx = 0;
+        gbc.anchor = GridBagConstraints.CENTER;
+
+        JPanel troudeballe = new JPanel();
+
+        TitledBorder titledBorder = BorderFactory.createTitledBorder("CrÃ©dits");
+        titledBorder.setTitleColor(Color.magenta);
+        titledBorder.setTitleFont( titledBorder.getTitleFont().deriveFont(Font.BOLD));
+        troudeballe.setBorder(BorderFactory.createCompoundBorder(
+                titledBorder,
+                BorderFactory.createEmptyBorder(5, 5, 5, 5)));
+
+        troudeballe.setLayout(new GridBagLayout());
+
+        gbc.ipadx = 10;
+        gbc.ipady = 7;
+
+        gbc.anchor = GridBagConstraints.EAST;
         gbc.gridy = 0;
+        gbc.gridx = 0;
+        troudeballe.add(new JLabel("VAIZAND Victor"), gbc);
+        gbc.gridx = 1;
+        troudeballe.add(Utils.createLink("GitHub", "https://github.com/FarmVivi"), gbc);
 
-
-        this.add(new JLabel("Crédits:"), gbc);
         gbc.gridy = 1;
-        this.add(Utils.createLink("Rouault Alban", "https://github.com/albanrouault"), gbc);
-        gbc.gridy = 2;
-        this.add(Utils.createLink("Vaizand Victor", "https://github.com/FarmVivi"), gbc);
-        gbc.gridy = 3;
-        this.add(Utils.createLink("Projet GitHub", "https://github.com/cnam-bfc/NFA006-Fractals"), gbc);
+        gbc.gridx = 0;
+        troudeballe.add(new JLabel("ROUAULT Alban"), gbc);
+        gbc.gridx = 1;
+        troudeballe.add(Utils.createLink("GitHub", "https://github.com/albanrouault"), gbc);
+
+        JPanel troudeballeV2 = new JPanel();
+
+        titledBorder = BorderFactory.createTitledBorder("Fractals");
+        titledBorder.setTitleColor(Color.magenta);
+        titledBorder.setTitleFont( titledBorder.getTitleFont().deriveFont(Font.BOLD));
+        troudeballeV2.setBorder(BorderFactory.createCompoundBorder(
+                titledBorder,
+                BorderFactory.createEmptyBorder(5, 5, 5, 5)));
+
+        gbc.ipady = 2;
+        gbc.gridy = 0;
+        gbc.gridx = 0;
+        troudeballeV2.add(Utils.createLink("Projet GitHub", "https://github.com/cnam-bfc/NFA006-Fractals"), gbc);
+
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.gridy = 0;
+        this.add(troudeballe, gbc);
+        gbc.gridy = 1;
+        this.add(troudeballeV2, gbc);
     }
 }
